623f3867fba3396f506dfef6e7135aa58b9bd067
antonioroque200OK <antoniocarlos.roque@gmail.com>
2023-05-17 15:14:12 -0300

    Created entities Chat, Message and Model in service

---
+++ chatservice/internal/domain/entity/message.go
@@ -0,0 +1,45 @@
+package entity
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Message struct {
+	ID        string
+	Role      string
+	Content   string
+	Tokens    int
+	Model     *Model
+	CreatedAt time.Time
+}
+
+func NewMessage(role, content string, model *Model) (*Message, error) {
+
+	msg := &Message{
+		ID:        uuid.New().String(),
+		Role:      role,
+		Content:   content,
+		Model:     model,
+		CreatedAt: time.Now(),
+	}
+	if error := msg.Validate(); error != nil {
+		return nil, error
+	}
+	return msg, nil
+}
+
+func (m *Message) Validate() error {
+	if m.Role != "user" && m.Role != "system" && m.Role != "assistant" {
+		return errors.New("invalid role")
+	}
+	if m.Content == "" {
+		return errors.New("content is empty")
+	}
+	if m.CreatedAt.IsZero() {
+		return errors.New("invalid created at")
+	}
+	return nil
+}
